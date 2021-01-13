9a895805e192d5ea0a994cb94c50c58cc4865da9
sealfury <willspecker@gmail.com>
2021-01-13 15:21:11 +0100

    Adds axios, further styling, moving main screen functionality to separate component

--- App.jsx
+++ App.jsx
@@ -1,28 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { useSelector } from "react-redux";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-const MainScreen = () => {
-  const { appTitle } = useSelector((state) => state);
-  return (
-    <View style={styles.container}>
-      <Text>{appTitle}</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
-}
+import MainScreen from './components/MainScreen'
 
 const Stack = createStackNavigator()
 
