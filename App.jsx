287e1c13c5861e8b1b42feb8135414cca9278b40
sealfury <willspecker@gmail.com>
2021-01-13 14:48:42 +0100

    Adding react navigation packages updating App to use react navigation packages styling title bar

--- App.jsx
+++ App.jsx
@@ -2,6 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { useSelector } from "react-redux";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,13 +14,39 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = () => {
+const MainScreen = () => {
   const { appTitle } = useSelector((state) => state);
   return (
     <View style={styles.container}>
       <Text>{appTitle}</Text>
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+const Stack = createStackNavigator()
+
+const App = () => {
+  const { appTitle } = useSelector((state) => state);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Start" 
+          component={MainScreen} 
+          options={{ 
+            title: appTitle,
+            headerStyle: {
+              backgroundColor: 'purple'
+            },
+            headerTitleStyle: {
+              color: 'white',
+              fontSize: 22
+            }
+           }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
