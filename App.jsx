9ef67451794797efe64c7ca587b42a59fda3573a
sealfury <willspecker@gmail.com>
2021-01-15 14:40:33 +0100

    Adding text inputs to login page and a login button adding authentication module

--- App.jsx
+++ App.jsx
@@ -1,56 +1,65 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MainScreen from './app/components/MainScreen'
-import ArticleShow from './app/components/ArticleShow'
-import {Button} from 'react-native'
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MainScreen from "./app/components/MainScreen";
+import ArticleShow from "./app/components/ArticleShow";
+import LoginScreen from "./app/components/LoginScreen";
+import { Button } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 const App = () => {
-  const { appTitle, currentArticle } = useSelector((state) => state);
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Start" 
-          component={MainScreen} 
-          options={{ 
-            title: appTitle,
-            headerRight: () => (
-              <Button  title='Click me' />
-            ),
-            headerStyle: {
-              backgroundColor: 'black'
-            },
-            headerTitleStyle: {
-              color: 'white',
-              fontSize: 24,
-              fontFamily: 'Futura-CondensedMedium',
-              fontWeight: 'bold',
-            }
-           }}
-        />
-        <Stack.Screen 
-          name="ArticleShow"
-          component={ArticleShow}
-          options={{
-            title: currentArticle.title,
-            headerStyle: {
-              backgroundColor: 'black'
-            },
-            headerTitleStyle: {
-              color: 'white',
-              fontSize: 18,
-              fontFamily: 'Futura-CondensedMedium',
-              fontWeight: 'bold',
-            }
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	const { appTitle, currentArticle } = useSelector(state => state);
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Start"
+					component={MainScreen}
+					options={props => ({
+						title: appTitle,
+						headerRight: () => (
+							<Ionicons
+								name="person-add-outline"
+								onPress={() => props.navigation.navigate("Login")}
+                size={24}
+                color="white"
+                style={{marginRight: 18}}
+							/>
+						),
+						headerStyle: {
+							backgroundColor: "black",
+						},
+						headerTitleStyle: {
+							color: "white",
+							fontSize: 24,
+							fontFamily: "Futura-CondensedMedium",
+							fontWeight: "bold",
+						},
+					})}
+				/>
+				<Stack.Screen
+					name="ArticleShow"
+					component={ArticleShow}
+					options={{
+						title: currentArticle.title,
+						headerStyle: {
+							backgroundColor: "black",
+						},
+						headerTitleStyle: {
+							color: "white",
+							fontSize: 18,
+							fontFamily: "Futura-CondensedMedium",
+							fontWeight: "bold",
+						},
+					}}
+				/>
+				<Stack.Screen name="Login" component={LoginScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
--- app/components/LoginScreen.jsx
+++ app/components/LoginScreen.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const authenticateUser = () => {
+    
+  }
   return (
     <View>
-      <Text>Login screen</Text>
+      <Text style={{marginBottom: 24} } >Login screen</Text>
+      <TextInput 
+        onChangeText={(text) => setEmail(text)}
+        placeholder="Email"
+        style={styles.input}
+      />
+      <TextInput 
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Password"
+        style={styles.input}
+      />
+      <Button 
+        title="Log In"
+        reverseColor="green"
+        onPress={() => authenticateUser()}
+      />
     </View>
   )
 }
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: { padding: 9, margin: 18, height: 40, borderColor: "grey", borderWidth: 4, borderRadius: 8}
+})
